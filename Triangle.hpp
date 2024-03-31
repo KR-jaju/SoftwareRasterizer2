0063d71a14c09df17a83bf7b5602bd5f2a7ecbf6
jaju <78911247+KR-jaju@users.noreply.github.com>
2024-03-31 07:02:49 +0000

    Fix: corrected data

--- Triangle.hpp
+++ Triangle.hpp
@@ -8,9 +8,9 @@ class Triangle : public RenderInput<Vector4> {
 public:
 	Vector4	operator[](uint64_t idx) const {
 		static Vector4	data[] = {
-			Vector4(0.0, 1.0, 0.0, 0.0),
-			Vector4(0.0, 1.0, 0.0, 0.0),
-			Vector4(0.0, 1.0, 0.0, 0.0),
+			Vector4(0.0, 0.8, 0.0, 1.0),
+			Vector4(-0.8, -0.8, 0.0, 1.0),
+			Vector4(0.8, -0.8, 0.0, 1.0),
 		};
 		return (data[idx]);
 	}
