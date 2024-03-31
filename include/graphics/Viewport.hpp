9504e690112c402d258fd0d7930ff0e6982d96d0
jaju <78911247+KR-jaju@users.noreply.github.com>
2024-03-31 07:02:34 +0000

    Fix: Viewport transform

--- include/graphics/Viewport.hpp
+++ include/graphics/Viewport.hpp
@@ -14,7 +14,11 @@ public:
 	Number	y_max;
 	Viewport(Number x_min, Number y_min, Number x_max, Number y_max): x_min(x_min), y_min(y_min), x_max(x_max), y_max(y_max) {}
 	Vector2	transform(Vector4 ndc) const {
-		return Vector2(ndc.x * (this->x_max - this->x_min) + this->x_min, ndc.y * (this->y_max - this->y_min) + this->y_min);
+		Number	const xa = (x_max - x_min) * Number(0.5);
+		Number	const xb = (x_max + x_min) * Number(0.5);
+		Number	const ya = (y_max - y_min) * Number(0.5);
+		Number	const yb = (y_max + y_min) * Number(0.5);
+		return Vector2(ndc.x * xa + xb, -ndc.y * ya + yb);
 	}
 };
 
